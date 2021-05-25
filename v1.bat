2fd5af824ab48a9e8ca8d3c5dcc506b67d9e8111
dethsound <78589971+dethsound@users.noreply.github.com>
2021-05-25 08:58:54 -0700

    adding binary rick

--- v1.bat
+++ v1.bat
@@ -1,6 +1,5 @@
 @echo off
-cd Desktop\maker-virus--totally-not-a-rickroll-
 echo You ready? Here we go!
 timeout 10
 start https://www.youtube.com/watch?v=dQw4w9WgXcQ
-start rickroll.exe
+start virus.exe

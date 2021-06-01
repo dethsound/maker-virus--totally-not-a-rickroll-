c4d44f9278e3eab8cbe0ca2254cdbf1c8a3463b5
dethsound <78589971+dethsound@users.noreply.github.com>
2021-05-31 23:52:10 -0700

    v2.2
    added copyright restrictions to files made by me

--- v1.bat
+++ v1.bat
@@ -1,4 +1,5 @@
 @echo off
+echo Copyright (C) 2021  Eric Zarutsky
 cd C:\
 mkdir totallynotarickroll
 echo You ready? Here we go!
@@ -12,3 +13,16 @@ start rickroll.exe
 echo heheboi ok bye
 timeout 3
 exit
+REM 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
